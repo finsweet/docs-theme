bf2238ee23b521ccd67af4f90ae1522151a98de5
jaji oluwatobiloba <jajmantobi@gmail.com>
2023-02-24 14:28:33 -0500

    Code colors updated

---
+++ .changeset/short-jeans-shop.md
@@ -0,0 +1,5 @@
+---
+'@finsweet/docs-theme': patch
+---
+
+Update code colors and css import
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import './custom.css';
-
+// eslint-disable-next-line simple-import-sort/imports
 import DefaultTheme from 'vitepress/theme';
+import './custom.css';
 
 export default DefaultTheme;
